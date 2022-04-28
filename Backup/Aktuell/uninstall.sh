155da181a00ba61c5b523df8232ef6429dcc30b1
kreuber <kevin.reuber@gmail.com>
2022-04-28 20:40:53 +0200

    Add uninstall

---
+++ Backup/Aktuell/uninstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd ~
+
+docker-compose -f docker-compose/duplicati/docker-compose.yml down
+docker-compose -f docker-compose/watchtower/docker-compose.yml down
+
+sudo apt remove --purge -y docker-engine docker docker.io docker-ce docker-ce-cli containerd.io containerd runc
+sudo apt autoremove -y
+sudo apt autoclean
+
+sudo pip3 uninstall docker-compose
+
+rm -rf docker-compose
+rm -rf /home/duplicati
+sudo rm -rf /var/lib/docker
+sudo rm -rf /var/lib/containerd

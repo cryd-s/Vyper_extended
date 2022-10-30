6efc726831fe6a97f2101ca30f83aa72776a0c25
Robnex <97597150+Robnex@users.noreply.github.com>
2022-10-30 13:51:38 +0100

    Update setup_backupsystem.sh

--- Backup/Aktuell/setup_backupsystem.sh
+++ Backup/Aktuell/setup_backupsystem.sh
@@ -37,9 +37,7 @@ for i in $SERVICES; do
   if [[ ! -f $COMPOSE_DIR/$i/docker-compose.yml ]]; then
     if [[ "$i" == "duplicati" ]]; then
       cat << EOF > $COMPOSE_DIR/$i/docker-compose.yml
-#version: '3.8'
 services:
-#  duplicati:
     image: duplicati/duplicati
     restart: unless-stopped
     container_name: duplicati
@@ -51,9 +49,7 @@ services:
 EOF
     elif [[ "$i" == "watchtower" ]]; then
       cat << EOF > $COMPOSE_DIR/$i/docker-compose.yml
-#version: '3.8'
 services:
-#  watchtower:
     image: containrrr/watchtower
     restart: unless-stopped
     container_name: watchtower
@@ -65,9 +61,7 @@ services:
 EOF
     elif [[ "$i" == "portainer" ]]; then
       cat << EOF > $COMPOSE_DIR/$i/docker-compose.yml
-#version: '3.8'
 services:
-#  portainer:
     image: portainer/portainer-ce
     restart: unless-stopped
     container_name: portainer
